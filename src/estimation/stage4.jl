fab77615610cdabb516066347a9caa681777cb8a
Joseph Mullins <josephlyonmullins@gmail.com>
2024-02-14 15:09:03 -0600

    trying to fit everything. Hard!!!

--- src/estimation/stage4.jl
+++ src/estimation/stage4.jl
@@ -35,11 +35,19 @@ function data_moms(M,P)
         end
         stack(_)
     end
-    return [m0.value ; m1.value; m2.value]
+
+    m4 = @chain M begin
+        @transform :tD = :YDIV .- :YMAR :tF = :YBIRTH .- :YMAR
+        groupby(:edW)
+        @combine :div = mean(:YDIV.<2010) :tD10 = mean(:tD.<10) :tF5 = mean(:tF.<5) :tF10=mean(:tF.<10)
+        stack(_)
+    end
+    
+    return [m0.value ; m1.value; m2.value; m4.value]
 end
 
-function model_moms(TD,TF,D,Lsim)
-    moms = zeros(14)
+function model_moms(TD,TF,D,Lsim,edW)
+    moms = zeros(22)
     moms[1:4] .= (mean(Lsim[.!D].==2), 
             mean(Lsim[.!D].==1), 
             mean(Lsim[D].==2), 
@@ -48,14 +56,34 @@ function model_moms(TD,TF,D,Lsim)
     moms[8:10] .= (mean(TF.<x) for x in (2,4,6))
     I = TD.<9998
     moms[11:14] .= (mean((TD[I] .- TF[I]).<x) for x in (0,5,10,15))
+    
+    @views moms[15:18] .= (mean(TD[.!edW].<9998), mean(TD[edW].<9998), mean(TD[.!edW].<10), mean(TD[edW].<10))
+    @views moms[19:22] .= (mean(TF[.!edW].<5), mean(TF[edW].<5), mean(TF[.!edW].<10), mean(TF[edW].<10))
+     
     return moms
 end
+
+# function model_moms(TD,TF,D,Lsim)
+#     moms = zeros(14)
+#     moms[1:4] .= (mean(Lsim[.!D].==2), 
+#             mean(Lsim[.!D].==1), 
+#             mean(Lsim[D].==2), 
+#             mean(Lsim[D].==1))
+#     moms[5:7] .= (mean(TD.<x) for x in (5,10,15))
+#     moms[8:10] .= (mean(TF.<x) for x in (2,4,6))
+#     I = TD.<9998
+#     moms[11:14] .= (mean((TD[I] .- TF[I]).<x) for x in (0,5,10,15))
+#     return moms
+# end
+
+
 
 function update(x,θ,F)
     π_ω = 1/(1+exp(x[12]))
     Π_ω = transmat_ω(π_ω, F.N_ω)
     return (;θ..., α_l=exp(x[1]), σ_L=exp(x[2]),α_F=x[3], σ_F=exp(x[4]), α_ω=[x[5],exp(x[6])], α_νH0=x[7:8], α_νH1=x[9:10], σ_ω=exp(x[11]), π_ω, Π_ω)
 end
+
 
 function get_x(θ)
     return [log(θ.α_l);log(θ.σ_L);θ.α_F;log(θ.σ_F);[θ.α_ω[1],log(θ.α_ω[2])];θ.α_νH0;θ.α_νH1;log(θ.σ_ω);log(1/θ.π_ω - 1)]
@@ -72,17 +100,17 @@ function get_moments(θ,values,F,dat,legal)
     mod = (;θ,values,F)
     solve_all!(mod)
     TD,TF,L_sim, Ω_sim, D = data_gen(mod,dat,legal)
-    return model_moms(TD,TF,D,L_sim)
+    return model_moms(TD,TF,D,L_sim,dat.edW.==1)
 end
 
 function ssq(θ,values,F,moms0,dat,legal)
     moms1 = get_moments(θ,values,F,dat,legal)
     ssq_ = sum((moms1 .- moms0).^2)
     if isnan(ssq_)
-        println(Inf)
+        #println(Inf)
         return Inf
     else
-        println(ssq_)
+        #println(ssq_)
         return ssq_
     end
 end
@@ -91,10 +119,22 @@ function ssq(θ,wght,values,F,moms0,dat,legal)
     moms1 = get_moments(θ,values,F,dat,legal)
     ssq_ = sum(wght .* (moms1 .- moms0).^2)
     if isnan(ssq_)
-        println(Inf)
+        #println(Inf)
         return Inf
     else
-        println(ssq_)
+        #println(ssq_)
         return ssq_
     end
+end
+
+function estimate_blocks(θ,blocks,wght,values,F,moms0,dat,legal)
+    bi = 1
+    for b in blocks
+        println("Doing block $bi ...")
+        x0 = get_x(θ)[b]
+        r = optimize(x->ssq(update(x,b,θ,F),wght,values,F,moms0,dat,legal),x0,Optim.Options(iterations=50))
+        θ = update(r.minimizer,b,θ,F)
+        bi += 1
+    end
+    return θ
 end
--- tests/test_stage4.jl
+++ tests/test_stage4.jl
@@ -23,6 +23,9 @@ V = values(F);
 L = convert(Vector{Int64},P.L)
 moms0 = data_moms(M,P)
 
+dat = repeat(M,10)
+legal = repeat(L,10)
+
 # θ = (;θ...,
 #     σ_L = 2., α_l = 1.,
 #     α_F = 2.,σ_F = 10.,
@@ -33,14 +36,63 @@ moms0 = data_moms(M,P)
 θ = (;θ...,
     σ_L = 2., α_l = 1.,
     α_F = -2.,σ_F = 10.,
-    α_ω = [0., 5.],σ_ω = 4., # σ_ω = 2.
+    α_ω = [0., 5.],σ_ω = 3., # σ_ω = 2.
     π_ω = 0.7, Π_ω = transmat_ω(0.7,F.N_ω),
     α_νH0 = [3.,-0.1],α_νH1 = [7.,0.2])
 
 
-wght = [1.,1.,1.,1.,0.,0.,1.,0.,0.,1.,10.,0.,0.,1.]
+#wght = [1.,1.,1.,1.,0.,0.,1.,0.,0.,1.,50.,0.,0.,1.]
 x0 = get_x(θ)
-res1 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),x0,Optim.Options(iterations=200))
+
+# function update(x,θ,F)
+#     π_ω = 1/(1+exp(x[12]))
+#     Π_ω = transmat_ω(π_ω, F.N_ω)
+#     return (;θ..., α_l=exp(x[1]), σ_L=exp(x[2]),α_F=x[3], σ_F=exp(x[4]), α_ω=[x[5],exp(x[6])], α_νH0=x[7:8], α_νH1=x[9:10], σ_ω=exp(x[11]), π_ω, Π_ω)
+# end
+# blocks:
+# α_l, σ_L, 
+# α_ω[1],α_ω[2],α_νH0[1], α_νH1[1], π_ω, σ_ω
+# α_F,σ_F,α_ω[1],α_νH0[1]
+# α_ω[1], α_F, α_νH0, α_νH1, σ_ω,
+# wght = ones(14); wght[11] = 50.
+
+#res1 = optimize(x->ssq(update(x,θ,F),V,F,moms0,dat,legal),x0,BFGS(),Optim.Options(iterations=5,show_trace = true))
+#res1 = optimize(x->ssq(update(x,θ,F),V,F,moms0,dat,legal),x0,Optim.Options(iterations=20,show_trace = true))
+#θ = update(res1.minimizer,θ,F)
+#m1 = get_moments(θ,V,F,M,L)
+#display([moms0 m1])
+
+# blocks = [
+#     [5,6,7,9,11,12],
+#     [3,4,5,7],
+#     [3,5,7,8,9,10,11],
+#     [1,2]
+# ]
+# θ = estimate_blocks(θ,blocks,wght,V,F,moms0,M,L)
+# θ = estimate_blocks(θ,blocks,wght,V,F,moms0,M,L)
+
+
+#res1 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),x0,Optim.Options(iterations=200))
+#res2 = optimize(x->ssq(update(x,θ,F),V,F,moms0,M,L),res1.minimizer,Optim.Options(iterations=1000))
+#θ = update(res2.minimizer,θ,F)
+
+x0 = get_x(θ)
+
+wght = ones(22); wght[11] = 0.
+#res1 = optimize(x->ssq(update(x,θ,F),V,F,moms0,M,L),x0,Optim.Options(iterations=1000))
+res1 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),x0,Optim.Options(iterations=1000,show_trace=true))
+res1 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),res1.minimizer,Optim.Options(iterations=1000,show_trace=true))
+θ = update(res1.minimizer,θ,F)
+m1 = get_moments(θ,V,F,M,L)
+display([moms0 m1])
+
+break
+wght = 1 ./ (moms0 .* (1 .- moms0))
+res2 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),x0,Optim.Options(iterations=1000))
+
+
+break
+# x1 = get_x(θ)
 
 #V2 = values(F);
 #     return (;θ..., α_l=exp(x[1]), σ_L=exp(x[2]),α_F=x[3], σ_F=exp(x[4]), α_ω=[x[5],exp(x[6])], α_νH0=x[7:8], α_νH1=x[9:10], σ_ω=exp(x[11]), π_ω, Π_ω)
@@ -53,10 +105,9 @@ res1 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),x0,Optim.Options(itera
 θ = update(res1.minimizer,θ,F)
 
 # now go after the unweighted version:
-res2 = optimize(x->ssq(update(x,θ,F),V,F,moms0,M,L),res1.minimizer,Optim.Options(iterations=1000))
+# res2 = optimize(x->ssq(update(x,θ,F),V,F,moms0,M,L),res1.minimizer,Optim.Options(iterations=1000))
 #res2 = optimize(x->ssq(update(x,θ,F),V,F,moms0,M,L),res2.minimizer,Optim.Options(iterations=200))
 
-θ = update(res2.minimizer,θ,F)
 
 m1 = get_moments(θ,V,F,M,L)
 display([moms0 m1])
@@ -76,6 +127,11 @@ x1 = res1.minimizer;
 m1 = get_moments(θ2,V,F,M,L)
 display([moms0 m1])
 
+x1 = readdlm("/Users/joseph/Dropbox/Research Projects/BrownFlinnMullins/Code/model_current/est_s4")[:]
+θ = update(x1,θ,F)
+θ = (;θ...,α_l = 1.)
+moms1 = get_moments(θ,V,F,M,L)
+[moms0 moms1]
 
 
 # α_ω,σ_ω,α_F,σ_F,α_νH[0],α_νH1[1], π_ω, (too many parameters)
