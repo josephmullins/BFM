89ac9e817f46425cf86fb2fd45b8bd1ec7859cc2
Joseph Mullins <josephlyonmullins@gmail.com>
2024-02-14 20:03:20 -0600

    alternative moments for stage 4

--- src/estimation/stage4.jl
+++ src/estimation/stage4.jl
@@ -1,82 +1,43 @@
-function data_moms(M,P)
+function data_moms(P)
     # E[PT / FT|married / divorced]
-    m0 = @chain P begin
-        @subset .!ismissing.(:M_hrs)
-        groupby(:DIV)
-        @combine :PT = mean((:M_hrs.>1000) .& (:M_hrs.<2000)) :FT = mean(skipmissing(:M_hrs.>=2000))
-        stack(_) 
-        @orderby :DIV :variable
-    end
-    
-    # P[tD<x],for x = 5, 10, 15,
-    # P[tF<x] for x = 2, 4, 6
-    m1 = @chain M begin
-        @transform :tD = :YDIV .- :YMAR :tF = :YBIRTH .- :YMAR
-        @combine begin
-            :tD5 = mean(:tD.<5)
-            :tD10 = mean(:tD.<10)
-            :tD15 = mean(:tD.<15)
-            :tF2 = mean(:tF.<2)
-            :tF4 = mean(:tF.<4)
-            :tF6 = mean(:tF.<6)
+    m = @chain P begin
+        @transform :F = :YEAR .>= :YBIRTH
+        @subset :M_AGE.>=22 :M_AGE.<=40
+        groupby(:M_AGE)
+        @combine begin 
+            :PT = mean(skipmissing((:M_hrs.>0) .& (:M_hrs.<1750)))
+            :FT = mean(skipmissing(:M_hrs.>=1750))
+            :DIV = mean(:DIV)
+            :F = mean(:F)
+            :DF = mean(:DIV .* :F)
         end
         stack(_)
     end
-
-    # P[tD-tF<x|yD<=2010] for x in 0,5,10,15
-    m2 = @chain M begin
-        @subset :YDIV.<2010
-        @transform :tDF = :YDIV .- :YBIRTH
-        @combine begin
-            :tDF0 = mean(:tDF.<0)
-            :tDF5 = mean(:tDF.<5)
-            :tDF10 = mean(:tDF.<10)
-            :tDF15 = mean(:tDF.<15)
-        end
-        stack(_)
-    end
-
-    m4 = @chain M begin
-        @transform :tD = :YDIV .- :YMAR :tF = :YBIRTH .- :YMAR
-        groupby(:edW)
-        @combine :div = mean(:YDIV.<2010) :tD10 = mean(:tD.<10) :tF5 = mean(:tF.<5) :tF10=mean(:tF.<10)
-        stack(_)
-    end
-    
-    return [m0.value ; m1.value; m2.value; m4.value]
 end
 
-function model_moms(TD,TF,D,Lsim,edW)
-    moms = zeros(22)
-    moms[1:4] .= (mean(Lsim[.!D].==2), 
-            mean(Lsim[.!D].==1), 
-            mean(Lsim[D].==2), 
-            mean(Lsim[D].==1))
-    moms[5:7] .= (mean(TD.<x) for x in (5,10,15))
-    moms[8:10] .= (mean(TF.<x) for x in (2,4,6))
-    I = TD.<9998
-    moms[11:14] .= (mean((TD[I] .- TF[I]).<x) for x in (0,5,10,15))
-    
-    @views moms[15:18] .= (mean(TD[.!edW].<9998), mean(TD[edW].<9998), mean(TD[.!edW].<10), mean(TD[edW].<10))
-    @views moms[19:22] .= (mean(TF[.!edW].<5), mean(TF[edW].<5), mean(TF[.!edW].<10), mean(TF[edW].<10))
+function model_moms(TD,TF,Lsim,tlength,age_mar)
+    moms = zeros(19,5)
+    denom = zeros(19)
+    it = 1
+    for i in eachindex(TD)
+        am = age_mar[i]
+        for t in 1:tlength[i]
+            age_i = am+t - 22
+            if age_i>=1 && age_i<=19
+                d = t>=TD[i]+1
+                f = t>=TF[i]+1
+                ft = Lsim[it]==2
+                pt = Lsim[it]==1
+                denom[age_i] += 1
+                moms[age_i,:] .+= (pt,ft,d,f,d*f)
+            end
+            it += 1
+        end
+    end
+    moms ./= denom
      
-    return moms
+    return moms[:]
 end
-
-# function model_moms(TD,TF,D,Lsim)
-#     moms = zeros(14)
-#     moms[1:4] .= (mean(Lsim[.!D].==2), 
-#             mean(Lsim[.!D].==1), 
-#             mean(Lsim[D].==2), 
-#             mean(Lsim[D].==1))
-#     moms[5:7] .= (mean(TD.<x) for x in (5,10,15))
-#     moms[8:10] .= (mean(TF.<x) for x in (2,4,6))
-#     I = TD.<9998
-#     moms[11:14] .= (mean((TD[I] .- TF[I]).<x) for x in (0,5,10,15))
-#     return moms
-# end
-
-
 
 function update(x,θ,F)
     π_ω = 1/(1+exp(x[12]))
@@ -99,8 +60,8 @@ end
 function get_moments(θ,values,F,dat,legal)
     mod = (;θ,values,F)
     solve_all!(mod)
-    TD,TF,L_sim, Ω_sim, D = data_gen(mod,dat,legal)
-    return model_moms(TD,TF,D,L_sim,dat.edW.==1)
+    TD,TF,L_sim, _, _ = data_gen(mod,dat,legal)
+    return model_moms(TD,TF,L_sim,dat.tlength,dat.age_mar)
 end
 
 function ssq(θ,values,F,moms0,dat,legal)
--- tests/test_stage4.jl
+++ tests/test_stage4.jl
@@ -21,17 +21,13 @@ V = values(F);
 # solve_all!(mod)
 # model_moms(TD,TF,D,L_sim)
 L = convert(Vector{Int64},P.L)
-moms0 = data_moms(M,P)
+M[!,:age_mar] = M.YMAR .- M.YBIRTH_M
+age_mar = convert(Vector{Int64},M.YMAR .- M.YBIRTH_M)
+tlength = convert(Vector{Int64},M.tlength)
+moms0 = data_moms(P).value
 
 dat = repeat(M,10)
 legal = repeat(L,10)
-
-# θ = (;θ...,
-#     σ_L = 2., α_l = 1.,
-#     α_F = 2.,σ_F = 10.,
-#     α_ω = [1., 5.],σ_ω = 4., # σ_ω = 2.
-#     π_ω = 0.9, Π_ω = transmat_ω(0.9,F.N_ω),
-#     α_νH0 = [3.,0.],α_νH1 = [4.,0.1])
 
 θ = (;θ...,
     σ_L = 2., α_l = 1.,
@@ -44,108 +40,14 @@ legal = repeat(L,10)
 #wght = [1.,1.,1.,1.,0.,0.,1.,0.,0.,1.,50.,0.,0.,1.]
 x0 = get_x(θ)
 
-# function update(x,θ,F)
-#     π_ω = 1/(1+exp(x[12]))
-#     Π_ω = transmat_ω(π_ω, F.N_ω)
-#     return (;θ..., α_l=exp(x[1]), σ_L=exp(x[2]),α_F=x[3], σ_F=exp(x[4]), α_ω=[x[5],exp(x[6])], α_νH0=x[7:8], α_νH1=x[9:10], σ_ω=exp(x[11]), π_ω, Π_ω)
-# end
-# blocks:
-# α_l, σ_L, 
-# α_ω[1],α_ω[2],α_νH0[1], α_νH1[1], π_ω, σ_ω
-# α_F,σ_F,α_ω[1],α_νH0[1]
-# α_ω[1], α_F, α_νH0, α_νH1, σ_ω,
-# wght = ones(14); wght[11] = 50.
-
-#res1 = optimize(x->ssq(update(x,θ,F),V,F,moms0,dat,legal),x0,BFGS(),Optim.Options(iterations=5,show_trace = true))
-#res1 = optimize(x->ssq(update(x,θ,F),V,F,moms0,dat,legal),x0,Optim.Options(iterations=20,show_trace = true))
-#θ = update(res1.minimizer,θ,F)
-#m1 = get_moments(θ,V,F,M,L)
+res1 = optimize(x->ssq(update(x,θ,F),V,F,moms0,dat,legal),x0,Optim.Options(iterations=1000,show_trace=true))
+res1 = optimize(x->ssq(update(x,θ,F),V,F,moms0,dat,legal),res1.minimizer,Optim.Options(iterations=1000,show_trace=true))
+θ = update(res1.minimizer,θ,F)
+m1 = get_moments(θ,V,F,M,L)
 #display([moms0 m1])
 
-# blocks = [
-#     [5,6,7,9,11,12],
-#     [3,4,5,7],
-#     [3,5,7,8,9,10,11],
-#     [1,2]
-# ]
-# θ = estimate_blocks(θ,blocks,wght,V,F,moms0,M,L)
-# θ = estimate_blocks(θ,blocks,wght,V,F,moms0,M,L)
-
-
-#res1 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),x0,Optim.Options(iterations=200))
-#res2 = optimize(x->ssq(update(x,θ,F),V,F,moms0,M,L),res1.minimizer,Optim.Options(iterations=1000))
-#θ = update(res2.minimizer,θ,F)
-
-x0 = get_x(θ)
-
-wght = ones(22); wght[11] = 0.
-#res1 = optimize(x->ssq(update(x,θ,F),V,F,moms0,M,L),x0,Optim.Options(iterations=1000))
-res1 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),x0,Optim.Options(iterations=1000,show_trace=true))
-res1 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),res1.minimizer,Optim.Options(iterations=1000,show_trace=true))
-θ = update(res1.minimizer,θ,F)
-m1 = get_moments(θ,V,F,M,L)
-display([moms0 m1])
-
-break
-wght = 1 ./ (moms0 .* (1 .- moms0))
-res2 = optimize(x->ssq(update(x,θ,F),wght,V,F,moms0,M,L),x0,Optim.Options(iterations=1000))
-
-
-break
-# x1 = get_x(θ)
-
-#V2 = values(F);
-#     return (;θ..., α_l=exp(x[1]), σ_L=exp(x[2]),α_F=x[3], σ_F=exp(x[4]), α_ω=[x[5],exp(x[6])], α_νH0=x[7:8], α_νH1=x[9:10], σ_ω=exp(x[11]), π_ω, Π_ω)
-# block = [3,4,5,6,7,9,11,12]
-# moms1 = get_moments(θ,V,F,M,L)
-# [moms0 moms1]
-#ssq(θ,V,F,moms0,M,L)
-
-#wght = 1 ./ (moms0 .* (1 .- moms0))
-θ = update(res1.minimizer,θ,F)
-
-# now go after the unweighted version:
-# res2 = optimize(x->ssq(update(x,θ,F),V,F,moms0,M,L),res1.minimizer,Optim.Options(iterations=1000))
-#res2 = optimize(x->ssq(update(x,θ,F),V,F,moms0,M,L),res2.minimizer,Optim.Options(iterations=200))
-
-
-m1 = get_moments(θ,V,F,M,L)
-display([moms0 m1])
-
-break
-
-idx = LinearIndices((F.N_ϵ,F.N_ω,F.N_d,2,2,2));
-
-x1 = res1.minimizer;
-# doesn't seem to work
-θ2 = (;θ...,);
-θ2.α_ω[1] = 0.
-θ2.α_νH0[1] = 3.88
-θ2 = (;θ2...,α_F = -40. )
-θ2 = (;θ2..., π_ω = 0.9, Π_ω = transmat_ω(0.9,F.N_ω) )
-
-m1 = get_moments(θ2,V,F,M,L)
-display([moms0 m1])
-
-x1 = readdlm("/Users/joseph/Dropbox/Research Projects/BrownFlinnMullins/Code/model_current/est_s4")[:]
-θ = update(x1,θ,F)
-θ = (;θ...,α_l = 1.)
-moms1 = get_moments(θ,V,F,M,L)
-[moms0 moms1]
-
-
-# α_ω,σ_ω,α_F,σ_F,α_νH[0],α_νH1[1], π_ω, (too many parameters)
-# divorce bfore kids, divorce eventually, fertility eventually
-
-
-# # doesn't work that well, try the simultaneous version?
-# blocks = [1:2,3:4,5:6,7:11]
-# for b in blocks
-#     xcurrent = get_x(θ)
-#     x0 = xcurrent[b]
-#     res = optimize(x->ssq(update(x,b,θ,F),V,F,moms0,M,L),x0,Optim.Options(iterations=50))
-#     xcurrent[b] .= res.minimizer
-#     θ = update(xcurrent,θ,F)
-# end
-
-
+using Plots
+M1 = reshape(m1,19,5)
+M0 = reshape(moms0,19,5)
+plot!(22:40,M1,layout = (2,3))
+plot!(22:40,M0,layout = (2,3))
