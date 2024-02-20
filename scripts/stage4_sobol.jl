d9a5eee0edeeb37591d32548aae45dd4b54f89d5
Joseph Mullins <josephlyonmullins@gmail.com>
2024-02-20 06:15:54 -0600

    moved to a function. hope this helps!

--- scripts/stage4_sobol.jl
+++ scripts/stage4_sobol.jl
@@ -10,10 +10,13 @@ F = FixedParams()
 θ = Params(F)
 V = values(F);
 
+
 # this works, but we need to set up the sequence.
 θ = stage1(θ,F,P)
 θ = stage2(θ,K)
 θ = stage3(θ,K,F.τgrid,cprobs)
+
+mod = (;θ,values=V,F)
 
 L = convert(Vector{Int64},P.L)
 moms0 = data_moms(M,P)
@@ -27,25 +30,29 @@ legal = repeat(L,10)
 x_lb = get_x(θ_lb)
 x_ub = get_x(θ_ub)
 
-using Sobol
+using Sobol, DelimitedFiles
 s = SobolSeq(12)
-N = 10_000
 
-Qsave = zeros(N)
-Xsave = zeros(12,N)
-for n in 1:N
-    u = next!(s)
-    x = x_lb .+ u .* (x_ub .- x_lb)
-    Xsave[:,n] .= x
-    Qsave[n] = ssq(update(x,θ,F),V,F,moms0,dat,legal)
-    println(n," ",Qsave[n])
+function sobol_search(x_lb,x_ub,mod,moms0,dat,legal;N=10_000)
+    (;θ,values,F) = mod
+    Qsave = zeros(N)
+    Xsave = zeros(12,N)
+    for n in 1:N
+        u = next!(s)
+        x = x_lb .+ u .* (x_ub .- x_lb)
+        Xsave[:,n] .= x
+        Qsave[n] = ssq(update(x,θ,F),values,F,moms0,dat,legal)
+        println(n," ",Qsave[n])
+    end
+    ii = sortperm(Qsave)[1:10]
+    X = Xsave[:,ii]
+    return X,Qsave[ii]
 end
 
-using DelimitedFiles
-ii = sortperm(Qsave)[1:10]
-X = Xsave[:,ii]
+X,Q = sobol_search(x_lb,x_ub,mod,moms0,dat,legal;N = 10)
+
 writedlm("output/Xsave_round1",X)
-writedlm("output/Qsave_round1",Qsave[ii])
+writedlm("output/Qsave_round1",Q)
 
 # x0 = Xsave[:,argmin(Qsave)]
 # res1 = optimize(x->ssq(update(x,θ,F),V,F,moms0,dat,legal),x0,Optim.Options(iterations=200,show_trace=true))
@@ -56,19 +63,7 @@ writedlm("output/Qsave_round1",Qsave[ii])
 x_lb = [minimum(X[i,:]) for i in axes(X,1)]
 x_ub = [maximum(X[i,:]) for i in axes(X,1)]
 
-s = SobolSeq(12)
+X,Q = sobol_search(x_lb,x_ub,mod,moms0,dat,legal;N = 10)
 
-Qsave2 = zeros(N)
-Xsave2 = zeros(12,N)
-for n in 1:N
-    u = next!(s)
-    x = x_lb .+ u .* (x_ub .- x_lb)
-    Xsave2[:,n] .= x
-    Qsave2[n] = ssq(update(x,θ,F),V,F,moms0,dat,legal)
-    println(n," ",Qsave[n])
-end
-
-ii = sortperm(Qsave2)[1:10]
-X = Xsave2[:,ii]
 writedlm("output/Xsave_round2",X)
-writedlm("output/Qsave_round2",Qsave2[ii])
+writedlm("output/Qsave_round2",Q)
