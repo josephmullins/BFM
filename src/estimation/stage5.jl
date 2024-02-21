038d1df40db05cb183383920c96fa807e7033266
Joseph Mullins <josephlyonmullins@gmail.com>
2024-02-21 13:03:06 -0600

    code to estimate is working

--- src/estimation/stage5.jl
+++ src/estimation/stage5.jl
@@ -1,15 +1,14 @@
 # add to data frame so we can use a different status variable
 function kidmoms_data(d) #<-?
     m = @chain d begin
-        @subset :AGE.<=18
+        @subset :AGE.<=17
         @subset :AGE.>=3
-        #@transform :AGE = round(:AGE ./ 4)
-        groupby([:DIV,:AGE])
+        #@transform :AGE = round.(:AGE ./ 4)
+        groupby([:dgroup,:AGE])
         @combine :S = mean(skipmissing(:AP_raw))
     end
     return m
 end
-
 
 # TODO:
 # finish and test code to calculate test score moments in data (above)
@@ -17,3 +16,47 @@ end
 # write code to fetch the simulated moments
 # write code to get the objective
 # write code to minimize and see if it's enough
+# in data patterns, look at test score moments relative to never divorced group
+
+
+# TODO: fix this!!!
+function kid_moments(S,θk,θ,F,kid_data)
+    predict_k!(S,θk,θ,F,kid_data)
+    return [mean(S[kid_data.AK.==a .&& kid_data.G.==g]) for g in 1:3 for a in 3:17]
+end
+
+function obj_stage5(S,θk,θ,F,kid_data,kmoms0)
+    kmoms1 = kid_moments(S,θk,θ,F,kid_data)
+    return sum((kmoms1 .- kmoms0).^2)
+end
+
+function updateθk(x,θk,θ)
+    (;γ_ψ,δW,δH,Γa) = θk
+    (;αΓ_τWa,αΓ_τHa,β) = θ
+    γ_ψ0 = x[1]
+    γ_ψ[:] .= x[2:5]
+    δk = exp(x[6])/(1+exp(x[6]))
+    get_Γa!(Γa,δk,β)
+    δW[1] = exp(x[7])
+    δH[1] = exp(x[8])
+    for a in eachindex(δW)
+        δW[a] = δW[1] * αΓ_τWa[a] * Γa[2] / (αΓ_τWa[1] * Γa[a+1])
+        δH[a] = δH[1] * αΓ_τHa[a] * Γa[2] / (αΓ_τHa[1] * Γa[a+1])
+    end
+    return (;θk...,γ_ψ0,δk)
+end
+
+function get_Γa!(Γa,δk,β)
+    Γ_next = 1/(1-β) 
+    for a in reverse(eachindex(Γa))
+        Γa[a] = 1 + β * δk * Γ_next
+        Γ_next = Γa[a]
+    end
+end
+
+# recall the recursive formula:
+# Γ_{s,a} = δ_{s,a} * β Γ_{a+1}
+# Γ_{a} = 1 + β * δk* Γ_{a+1}
+# so:
+# δ_{s,a+1} = δ_{s,a} * Γ_{s,a+1} * Γ_{a+1} / (Γ_{s,a} * Γ_{a+2})
+# Γ_{a+1} / Γ_{a+2} = (1 + β * δ_{k} * Γ_{a+2}) / Γ_{a+2}
--- tests/test_stage5.jl
+++ tests/test_stage5.jl
@@ -8,6 +8,9 @@ P = CSV.read("data/MarriagePanel.csv",DataFrame,missingstring="NA")
 K = CSV.read("data/KidPanel.csv",DataFrame,missingstring="NA")
 cprobs = CSV.read("data/CustodyMomentsSimple.csv",DataFrame).p[:]
 
+K[!,:dgroup] .= 1 .+ (K.YDIV.<9000) .+ (K.TSD.>=0) #<- THIS NEEDS TO GO WITH THE DATA PREP!!!
+
+
 F = FixedParams()
 θ = Params(F)
 V = values(F);
@@ -33,10 +36,45 @@ sim_data = data_gen(mod,dat);
 kid_data = prep_child_data(sim_data,dat,cprobs);
 
 S = zeros(length(kid_data.ΩK))
-θk = (;γ_ψ0 = -5,γ_ψ = [0.,0.,0.2,0.],δW = fill(0.1,18),δH = fill(0.1,18),δk = 0.9)
-predict_k!(S,θk,θ,F,kid_data)
+θk = (;γ_ψ0 = -5,
+    γ_ψ = [0.,0.,0.2,0.],
+    δW = fill(0.1,18),δH = fill(0.1,18),δk = 0.9,
+    Γa = zeros(19))
 
-# here are the moments :-)
-Sm = [mean(S[kid_data.AK.==a .&& kid_data.G.==g]) for a in 3:16, g in 1:3]
+x0 = [-5.,0.,0.,0.2,0.,1.,log(0.1),log(0.1)]
+θk =  updateθk(x0,θk,θ)
+
+# the data moments:
+k = kidmoms_data(K)
+kmoms0 = k.S
 
 # we can do the same thing for the data easily enough
+obj_stage5(S,θk,θ,F,kid_data,kmoms0)
+
+res = optimize(x->obj_stage5(S,updateθk(x,θk,θ),θ,F,kid_data,kmoms0),x0,Optim.Options(show_trace = true))
+
+θk = updateθk(res.minimizer,θk,θ)
+kmoms1 = kid_moments(S,θk,θ,F,kid_data)
+
+# now we just need the update function and we're ready to go
+k[!,:Smod] .= kmoms1
+
+using StatsPlots
+@df stack(k) plot(
+    :AGE,
+    :value,
+    group = (:dgroup,:variable),
+    linecolor = :dgroup
+)
+
+p = plot()
+K0 = reshape(kmoms0,15,3)
+K1 = reshape(kmoms1,15,3)
+clrs = ["blue","red"]
+for i in 2:3
+    plot!(p,K0[:,i] .- K0[:,1],linecolor=clrs[i-1])
+    plot!(p,K1[:,i] .- K1[:,1],linecolor=clrs[i-1],linestyle=:dash)
+end
+p
+
+
