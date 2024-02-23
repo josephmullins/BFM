95cc29ba59544109bf3919ff2a256209ebfba4d1
Joseph Mullins <josephlyonmullins@gmail.com>
2024-02-23 09:02:10 -0600

    #

--- scripts/run_estimation.jl
+++ scripts/run_estimation.jl
@@ -16,22 +16,13 @@ V = values(F);
 x4_0 = readdlm("output/Xsave_round2")[:,1]
 x5_0 = [-20.,0.75,0.75,0.05,0.00,2.5,log(0.2),log(0.7),log(20.)]
 
-# θ = stage1(θ, F, P)
-# θ = stage2(θ, K)
-# θ = stage3(θ, K, F.τgrid, θ.cprobs)
-# θ = stage4(x4_0,θ,V,F,M,P; R = 10, num_iter = 5)
-# dat = prep_sim_data(M, P; R = 10)
-# mod = (;θ,values=V,F)
-# sim_data = data_gen(mod,dat);
-# kid_data = prep_child_data(sim_data,dat,cprobs);
-# S = zeros(length(kid_data.ΩK))
-
-# kmoms0 = kidmoms_data(K)
-
-# kmoms1 = kid_moments(S,θk,θ,F,kid_data)
-
-# θk = stage5(x5_0, θk, mod, dat, K; num_iter = 5)
-
-
-
 θ,θk = estimate_model(x4_0, x5_0, θ, θk, V, F, M, P ; R = 10, num_iter = 5, show_trace = true)
+x1,x2,x3,x4,x5 = stack_ests(θ,θk)
+writedlm("output/est_stage1",x1)
+writedlm("output/est_stage2",x2)
+writedlm("output/est_stage3",x3)
+writedlm("output/est_stage4",x4)
+writedlm("output/est_stage5",x5)
+# next: run this again to make sure it all works.
+
+# then: how shall we write up the results? in a jupyter notebook? or just go for the tables?
