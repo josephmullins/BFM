77ddd70d977b5b3a1d6207268461cc773c775165
Joseph Mullins <josephlyonmullins@gmail.com>
2024-04-19 08:03:13 -0500

    fixed a bug in stage 3 had wrong interpretation of regression

--- src/estimation/stage3.jl
+++ src/estimation/stage3.jl
@@ -1,12 +1,15 @@
+# E[τ|a,D] = Γ_a*(1-D) + Γ_a*E[1/(1+ρ(1-τbar))]*D
+# so we regress τ on Γ_a*β_1+ β_2*Γ_a*D and get:
+# E[1/(1+ρ(1 - τbar))] = 1+β_2
 function stage3(d,τgrid,cprobs)
     m = @chain d begin
         @subset .!ismissing.(:tau_f) :AGE.<=17
         groupby(:AGE)
         @transform :gamma_a = mean(:tau_f[.!:DIV])
         reg(@formula(tau_f ~ gamma_a + gamma_a&DIV + fe(KID)))
-        coef(_)[2]
+        1+coef(_)[2]
     end
-    modratio(ρ,τgrid,p) = dot(log.(1 ./ (1 .+ ρ.*(1 .- τgrid))),p)
+    modratio(ρ,τgrid,p) = dot((1 ./ (1 .+ ρ.*(1 .- τgrid))),p)
     res = optimize(ρ->(m - modratio(ρ,τgrid,cprobs))^2,0,10)
     ρ_est = res.minimizer
     return ρ_est
