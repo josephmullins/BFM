b00cd7c578216baf9ecd061f9f4b5f9aaf907f2d
Joseph Mullins <josephlyonmullins@gmail.com>
2024-05-06 16:47:06 -0500

    updates to number of iterations

--- src/estimation/estimation.jl
+++ src/estimation/estimation.jl
@@ -15,7 +15,7 @@ function estimate_model(x4_0,x5_0,θ,θk,V,F,data,panel,kid_data; R = 10, num_it
     θ = stage4(x4_0,θ,V,F,data,panel; R, num_iter, show_trace, seed = seed4)
     dat = prep_sim_data(data, panel; R)
     mod = (;θ,values=V,F)
-    θk = stage5(x5_0, θk, mod, dat, kid_data; num_iter, show_trace, seed = seed5)
+    θk = stage5(x5_0, θk, mod, dat, kid_data; num_iter=1000, show_trace, seed = seed5)
     return θ,θk
 end
 
--- tests/test_bootstrap.jl
+++ tests/test_bootstrap.jl
@@ -29,3 +29,30 @@ writedlm("output/boot_stage2",x2)
 writedlm("output/boot_stage3",x3)
 writedlm("output/boot_stage4",x4)
 writedlm("output/boot_stage5",x5)
+
+
+seed0 = 1010
+seed1 = 2020
+seed2 = 3030
+x4_0 = get_x(θ)
+x5_0 = get_xk(θk)
+x1,x2,x3,x4,x5 = stack_ests(θ,θk)
+X1b = zeros(length(x1),num_boot)
+X2b = zeros(length(x2),num_boot)
+X3b = zeros(length(x3),num_boot)
+X4b = zeros(length(x4),num_boot)
+X5b = zeros(length(x5),num_boot)
+b = 1
+println("Performing bootstrap trial $b")
+Random.seed!(seed0+b)
+Mb,Pb,Kb = draw_boot_sample(M,P,K)
+seed4 = seed1+b
+seed5 = seed2+b
+θ,θk = estimate_model(x4_0, x5_0, θ, θk, V, F, Mb, Pb, Kb ; R=10, num_iter=100, show_trace=true, seed4, seed5)
+dat = prep_sim_data(Mb, Pb; R=10)
+mod = (;θ,values=V,F)
+θk_1 = deepcopy(θk)
+θk_2 = deepcopy(θk)
+
+θk = stage5(x5_0, θk, mod, dat, Kb; num_iter=1000, show_trace=true, seed = seed5)
+
