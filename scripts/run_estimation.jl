a5e1be82581059e4874508aef6105dac974ddd48
Joseph Mullins <55055971+josephmullins@users.noreply.github.com>
2024-08-26 14:40:48 -0500

    Fix to data use

--- scripts/run_estimation.jl
+++ scripts/run_estimation.jl
@@ -4,7 +4,7 @@ using DelimitedFiles
 
 M = CSV.read("data/MarriageFile.csv",DataFrame,missingstring="NA")
 P = CSV.read("data/MarriagePanel.csv",DataFrame,missingstring="NA")
-K = CSV.read("data/KidPanel.csv",DataFrame,missingstring="NA")
+K = CSV.read("data/KidPanelv2.csv",DataFrame,missingstring="NA")
 cprobs = CSV.read("data/CustodyMomentsSimple.csv",DataFrame).p[:]
 
 F = FixedParams()
