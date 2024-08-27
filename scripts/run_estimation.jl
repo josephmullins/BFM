520cc3cfdc203f6f7d8fa8c7b458c824148d1a5f
Joseph Mullins <55055971+josephmullins@users.noreply.github.com>
2024-08-27 11:57:49 -0500

    Going back to starting guess

--- scripts/run_estimation.jl
+++ scripts/run_estimation.jl
@@ -13,7 +13,7 @@ V = values(F);
 θ = (;θ...,cprobs)
 θk = prod_pars()
 
-x4_0 = readdlm("output/Xsave_round2")[:,1] #<- the best guess from the Sobol Search
+x4_0 = readdlm("output/Xsave_round2_old")[:,1] #<- the best guess from the Sobol Search
 #x5_0 = [-20.,0.75,0.75,0.05,0.00,2.5,log(0.2),log(0.7),log(20.)]
 x5_0 = [-15.,0.75,0.75,0.05,0.00,2.5,log(0.2),log(0.7),log(12.)] #<- slightly better guess
 
